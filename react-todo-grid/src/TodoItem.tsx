70b2a60387d8257156106b1236a93fab614f51f0
SamVisualCare <143374106+SamVisualCare@users.noreply.github.com>
2023-12-01 14:21:48 +1030

    Added some styling to the todo list and items

--- react-todo-grid/src/TodoItem.tsx
+++ react-todo-grid/src/TodoItem.tsx
@@ -9,18 +9,19 @@ export interface TodoItemProps {
 }
 
 const TodoItem: React.FC<TodoItemProps> = ({ id, text, priority, urgency }) => {
-  const [, drag] = useDrag({
-    type: 'TODO_ITEM',
-    item: { id },
-  });
-
-  return (
-    <div ref={drag} style={{ padding: '8px', border: '1px solid #ddd', marginBottom: '4px' }}>
-      <strong>{text}</strong>
-      <div>Priority: {priority}</div>
-      <div>Urgency: {urgency}</div>
-    </div>
-  );
-};
+    const [, drag] = useDrag({
+      type: 'TODO_ITEM',
+      item: { id },
+    });
+  
+    return (
+      <div ref={drag} className="todo-item">
+        <strong>{text}</strong>
+        <div className="priority">Priority: {priority}</div>
+        <div className="urgency">Urgency: {urgency}</div>
+      </div>
+    );
+  };
+  
 
 export default TodoItem;
